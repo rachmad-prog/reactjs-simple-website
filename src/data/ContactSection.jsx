fcdb694166ad000628d24429eb1818e0af8bfcf7
Rachmad-Prog <rachmadmap@gmail.com>
2026-06-11 18:47:37 +0700

    Update ContactSection.jsx

--- src/data/ContactSection.jsx
+++ src/data/ContactSection.jsx
@@ -10,7 +10,7 @@ export const contactSection = [
      {
         content:`<h3>Contact</h3>
                         <p>Jl. Ciheuleut Pakuan Baranangsiang Kota Bogor</p>
-                        <a href="https://wa.me/6285162701391"><p>Phone Number: +62851-6270-1391</p>`
+                        <p>Phone Number: +62851-6270-1391</p>`
     },
      {
         content:`<h3>Social</h3>
